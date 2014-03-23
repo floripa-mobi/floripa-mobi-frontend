c7687fdad8b5cf1f40d3ec60902e04d1a68f3241
Paulo Ragonha <paulo@ragonha.me>
2014-03-22 22:27:40 -0300

    rendering schedule (only first 3 hours of the day)

--- app/bus_detail.js
+++ app/bus_detail.js
@@ -1,7 +1,29 @@
 define([
-  'react'
+  'react',
+  'underscore'
 ],
-function (React) {
+function (React, _) {
+  var Schedule = React.createClass({
+    render: function () {
+      var schedule = this.props.schedule,
+          past = schedule.schedule[0],
+          present = schedule.schedule[1],
+          future = schedule.schedule[2];
+
+      return (
+        <div className='schedule'>
+          <h2>{schedule.departure}</h2>
+          <ol>
+            <li className='hour past'>{past}</li>
+            <li className='hour present'>{present}</li>
+            <li className='hour future'>{future}</li>
+          </ol>
+        </div>
+      )
+    }
+  });
+
+
   var BusDetail = React.createClass({
     close: function () {
       this.props.onClose();
@@ -9,15 +31,42 @@ function (React) {
 
     render: function () {
       var bus = this.props.bus,
+          schedules = getCurrentSchedules(bus.schedule),
           className = 'bus-detail ' + this.props.className;
 
       return (
         <div className={className} onClick={this.close}>
           <h1>{bus.name}</h1>
+          {
+            _(schedules).map(function (schedule) {
+              return <Schedule schedule={schedule}></Schedule>
+            })
+          }
         </div>
       );
     }
   });
+
+
+  function getCurrentSchedules (schedules) {
+    var period = getCurrentPeriod();
+
+    console.log(period)
+
+    return _(schedules).filter(function (schedule) {
+      return schedule.period === period;
+    });
+  }
+
+
+  function getCurrentPeriod () {
+    var dayOfTheWeek = new Date().getDay();
+
+    if (dayOfTheWeek === 0) { return 'sunday'; }
+    if (dayOfTheWeek === 6) { return 'saturday'; }
+    return 'weekday';
+  }
+
 
   return BusDetail;
 });
--- app/services/bus_detail_service.js
+++ app/services/bus_detail_service.js
@@ -6,12 +6,12 @@ function () {
       number: "177",
       name: "Santa Monica",
       schedule: [
-        { departure: 'titri', weekDays: 'uteis', schedule: ['10:23', '10:30'] },
-        { departure: 'titri', weekDays: 'sabado', schedule: ['10:23', '10:30'] },
-        { departure: 'titri', weekDays: 'domingo', schedule: ['10:23', '10:30'] },
-        { departure: 'ufsc', weekDays: 'uteis', schedule: ['10:23', '10:30'] },
-        { departure: 'ufsc', weekDays: 'sabado', schedule: ['10:23', '10:30'] },
-        { departure: 'ufsc', weekDays: 'domingo', schedule: ['10:23', '10:30'] }
+        { departure: 'TITRI -> UFSC - Ida', period: 'weekday', schedule: ['10:23', '10:30', '10:50'] },
+        { departure: 'TITRI -> UFSC - Ida', period: 'saturday', schedule: ['10:23', '10:30', '10:50'] },
+        { departure: 'TITRI -> UFSC - Ida', period: 'sunday', schedule: ['10:23', '10:30', '10:50'] },
+        { departure: 'UFSC -> TITRI - Volta', period: 'weekday', schedule: ['10:23', '10:30', '10:50'] },
+        { departure: 'UFSC -> TITRI - Volta', period: 'saturday', schedule: ['10:23', '10:30', '10:50'] },
+        { departure: 'UFSC -> TITRI - Volta', period: 'sunday', schedule: ['10:23', '10:30', '10:50'] }
       ]
     });
   }
